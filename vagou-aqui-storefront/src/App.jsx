c241b1b04e4c6b60d915a9399200346c9033e039
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-09 15:02:57 -0300

    added return component and missing import statements.

--- vagou-aqui-storefront/src/App.jsx
+++ vagou-aqui-storefront/src/App.jsx
@@ -1,8 +1,11 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductList from './ProductList';
 
 const App = () => {
+
     const [loading, setLoading] = useState(false);
     const [productList, setProductList] = useState([]);
+    const [categories, setCategories] = useState([]);
     const [selectedCategories, setSelectedCategories] = useState([]);
     const [fileredProductList, setFilteredProductList] = useState([]);
 
@@ -14,12 +17,13 @@ const App = () => {
 
     const removeCategory = (category) => {
         if(selectedCategories.includes(category)){
+            console.log(selectedCategories)
             const removedList = selectedCategories.filter((item) => (item !== category));
             setSelectedCategories(removedList);
         }
     }
 
-    const resetCategory = () => { // this function will be used to clear the filter
+    const resetCategory = () => {
         setSelectedCategories([]);
     }
 
@@ -35,35 +39,65 @@ const App = () => {
 
         await fetch('https://dummyjson.com/products/categories')
             .then(res => res.json())
-            .then(data =>
+            .then(data => {
                 setCategories(data);
-    })
-.catch(err => alert(err))
-        .finally(()=>{
-            setLoading(false);
-        })
-}
-const getProducts = async () => {
-    setLoading(true);
+            })
+            .catch(err => alert(err))
+            .finally(()=>{
+                setLoading(false);
+            })
+    }
 
-    await fetch('https://dummyjson.com/products')
-        .then(res => res.json())
-        .then(data => .then(data => {
-            setProductList(data.products);
-            getCategories(); // get the categories list
-        })
-        .catch(err => alert(err))
-        .finally(()=>{
-            setLoading(false);
-        })
-}
+    const getProducts = async () => {
+        setLoading(true);
 
-useEffect(() => {
-    getProducts();
-}, [])
+        await fetch('https://dummyjson.com/products')
+            .then(res => res.json())
+            .then(data => {
+                setProductList(data.products);
+                setFilteredProductList(data.products);
+                getCategories(); // get the categories list
+            })
+            .catch(err => alert(err))
+            .finally(()=>{
+                setLoading(false);
+            })
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, [])
 
     return (
-        <div>App</div>
+        <div className='w-screen h-screen px-5 bg-gray-100 flex justify-center items-center'>
+            <div className='w-full h-[90%] rounded-md bg-white'>
+                <div className='relative w-full h-[15%] flex items-center overflow-x-auto'>
+                    <span className='mx-3 ml-5 font-medium'> Categories: </span>
+                    {
+                        categories.map((category) => (
+                            <div
+                                onClick={() => {
+                                    if(selectedCategories.includes(category)){
+                                        removeCategory(category);
+                                    } else{
+                                        addCategory(category);
+                                    }
+                                }}
+                                className={`w-fit min-w-fit h-8 mx-2 px-5 py-2 flex flex-row justify-center items-center text-sm border break-keep rounded-3xl cursor-pointer transition-all duration-300 ${(selectedCategories.includes(category))?'border-blue-500 bg-blue-500 text-white':' border-gray-500 bg-white text-gray-900'} `}>
+                                {category.split("-").join(" ")}
+                            </div>
+                        ))
+                    }
+                    <div
+                        onClick={() => resetCategory()}
+                        className={`${(selectedCategories.length>0)?'opacity-100':'opacity-0 pointer-events-none'} sticky right-0 w-fit h-full px-5 flex justify-center items-center text-blue-500 bg-white backdrop-blur-lg cursor-pointer hover:text-blue-700 transition-all duration-300`}
+                    >
+                        clear
+                    </div>
+                </div>
+                <ProductList filteredProductList={fileredProductList} loading={loading} />
+            </div>
+        </div>
     )
 }
 export default App
