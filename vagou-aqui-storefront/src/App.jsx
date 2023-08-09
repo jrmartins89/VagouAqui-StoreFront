17c5f501a21026b821a538422de24caa8b1e10f3
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-09 14:52:37 -0300

    added function to fetch dummy content from API.

--- vagou-aqui-storefront/src/App.jsx
+++ vagou-aqui-storefront/src/App.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
 
 const App = () => {
+    const [loading, setLoading] = useState(false);
+
+    const getCategories = async () => {
+        setLoading(true);
+
+        await fetch('https://dummyjson.com/products/categories')
+            .then(res => res.json())
+            .then(data =>
+                setCategories(data);
+    })
+.catch(err => alert(err))
+        .finally(()=>{
+            setLoading(false);
+        })
+}
+
     return (
         <div>App</div>
     )
